878e744ecbe809ae6931766e572ef721e2ec0268
Kettel <75881317+Kettel1@users.noreply.github.com>
2022-03-12 16:53:37 +0300

    Начало проекта - Тестовый комит из ветки

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App/App';
 import {BrowserRouter} from "react-router-dom";
+import './index.scss'
 
 ReactDOM.render(
     <React.StrictMode>
